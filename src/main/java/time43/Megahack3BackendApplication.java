373024731a85ea319c331bf440d0c82ee07e47d8
eduardorcury <eduardoribeirocury@gmail.com>
2020-07-02 11:45:36 -0400

    Commit inicial - Projeto Criado

---
+++ src/main/java/time43/Megahack3BackendApplication.java
@@ -0,0 +1,13 @@
+package time43;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Megahack3BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Megahack3BackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/time43/Megahack3BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package time43;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Megahack3BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

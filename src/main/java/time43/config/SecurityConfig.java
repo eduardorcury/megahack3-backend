8d8d1810ac35fcaa073f66f2139ecb210a5c9958
eduardorcury <eduardoribeirocury@gmail.com>
2020-07-03 15:24:19 -0400

    Documentação com swagger

--- src/main/java/time43/config/SecurityConfig.java
+++ src/main/java/time43/config/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -30,6 +31,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	private JWTUtil jwtUtil;
 	
 	private static final String[] PUBLIC_MATCHERS = { "/", "/bares/**", "/enderecos/**" };
+	
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring().antMatchers("/v2/api-docs", "/configuration/ui", "/swagger-resources/**", "/configuration/**",
+			"/swagger-ui.html", "/webjars/**");
+	}
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
